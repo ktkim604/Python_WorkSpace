ef5f364613f939d3c7ea7a3b95b0a070da5c1f40
Kyutae Kim <kqt99@naver.com>
2022-06-28 00:02:11 +0900

    Update 8958.py

--- BOJ/8958.py
+++ BOJ/8958.py
@@ -1,17 +1,17 @@
 n = int(input())
 
-for i in range(5):
+for i in range(n):
     a = input()
     cnt = 0
     sum = 0
-    total = 0
+    base = 1
+    
     for j in a:
         if j == "O":
+            sum = sum + base + cnt
             cnt += 1
-            sum += cnt
-            total += sum 
+            
         elif j == "X":
-            cnt += 0
-            sum = 0
-            total = 0
-    print(total)
+            cnt = 0
+            
+    print(sum)
