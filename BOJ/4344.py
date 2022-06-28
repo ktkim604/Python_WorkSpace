022d42453c8e9a10c7819d93a9833a8e0e8bf0eb
Kyutae Kim <kqt99@naver.com>
2022-06-29 02:08:40 +0900

    Create 4344.py

---
+++ BOJ/4344.py
@@ -0,0 +1,13 @@
+from numpy import percentile
+
+
+C = int(input())
+for i in range(C):
+    scores = list(map(int, input().split()))
+    avg = sum(scores[1:])/scores[0]
+    cnt = 0
+    for j in scores[1:]:
+        if avg < j:
+            cnt += 1
+    percent = (cnt/scores[0])*100
+    print('%.3f' %percent + '%')
