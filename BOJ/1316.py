f5eeeb8a339e1834ef6eaaae6055bc800d2e1932
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-07-04 01:54:53 +0900

    Update 1316.py

---
+++ BOJ/1316.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = n
+
+for i in range(n):
+    t = input()
+    for j in range(len(t)-1):
+        
+        if t[j] in t[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
