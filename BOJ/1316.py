2015a68e79882c5d87efac49809d0912a681ac3c
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-07-05 19:08:57 +0900

    Update 1316.py

--- BOJ/1316.py
+++ BOJ/1316.py
@@ -4,8 +4,9 @@ cnt = n
 for i in range(n):
     t = input()
     for j in range(len(t)-1):
-        
-        if t[j] in t[j+1:]:
+        if t[j] == t[j+1]:
+            pass
+        elif t[j] in t[j+1:]:
             cnt -= 1
             break
 print(cnt)
