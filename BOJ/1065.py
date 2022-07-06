7b1b20c987023452dc48965834060066f74bfd38
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-07-07 00:18:32 +0900

    bog

--- BOJ/1065.py
+++ BOJ/1065.py
@@ -1,18 +1,18 @@
-# # 한수
+# 한수
 
-# def hansu(num):
-#     cnt = 0
-#     for i in range(1, num+1):
-#         if i <100:
-#             cnt += 1
-#         else:
-#             nums = list(map(int, str(i)))
-#             if nums[1]-nums[0] == nums[2]-nums[1]:
-#                 cnt += 1
-#     return cnt
-# num = int(input())
-# print(hansu(num))
+def hansu(num):
+    cnt = 0
+    for i in range(1, num+1):
+        if i <100:
+            cnt += 1
+        else:
+            nums = list(map(int, str(i)))
+            if nums[1]-nums[0] == nums[2]-nums[1]:
+                cnt += 1
+    return cnt
+num = int(input())
+print(hansu(num))
 
-for i in range(100, 150):
-    nums = list(map(int, str(i)))
-    print(nums)
+# for i in range(100, 150):
+#     nums = list(map(int, str(i)))
+#     print(nums)
