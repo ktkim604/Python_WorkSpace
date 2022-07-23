f6d35e217daefaaf1abf70fe1f98cfb1dad529ab
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-07-23 22:29:18 +0900

    boj

---
+++ BOJ/1978.py
@@ -0,0 +1,18 @@
+
+n = int(input())
+k = list(map(int, input().split()))
+
+result = 0
+
+for i in k:
+    cnt = 0
+    for j in range(1, i+1):
+        if (i % j) == 0:
+            cnt += 1
+            print(cnt, i, j)
+        
+    if cnt == 2:
+        result += 1
+        
+print(result)
+
