b437e3602a6c53ed57360ae6ddf8a021c1a77cf7
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-07-25 17:55:07 +0900

    boj

--- BOJ/2869.py
+++ BOJ/2869.py
@@ -1,17 +1,9 @@
+import math
+
 A, B, V = map(int, input().split())
 
-day = 0
-height = 0
-
-while True:
-    day += 1
-    height += A
-    if height >= V:
-        break
-    height -= B
-
-    
-print(day)
+day = (V-B)/(A-B)
+print(math.ceil(day))
     
     
     
