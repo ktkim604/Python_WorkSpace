553dba5e79f13617de5a82dfe9ca730a33827c83
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-08-19 16:01:10 +0900

    boj

---
+++ BOJ/11725.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    
+    while q:
+        v = q.popleft()
+        
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = v
+                q.append(i)
+    
+
+
+n = int(input())
+graph = [ [] for i in range(n+1)]
+
+for i in range(1, n):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+visited = [0] * (n+1)
+bfs(1)
+
+for i in range(2, n+1):
+    print(visited[i])
+    
--- BOJ/7562.py
+++ BOJ/7562.py
@@ -1,7 +1,5 @@
 from collections import deque
-from contextlib import AsyncExitStack
 import sys
-
 
 def bfs(x, y):
     q = deque()
@@ -12,29 +10,27 @@ def bfs(x, y):
     while q:
         cnt, x, y = q.popleft()
         if x == end_x and y == end_y:
-            print(cnt)
-            break
+            return cnt
+        
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < l and 0 <= ny < l:
-                if not visited[nx][ny]:
-                    
-                    visited[nx][ny] = 1
-                    q.append((cnt+1, nx, ny))
-                    
-    
-        
+            if 0 <= nx < l and 0 <= ny < l and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                q.append((cnt+1, nx, ny)) 
 
 
-t = int(input())
-for _ in range(t):
+
+
+for _ in range(int(input())):
     l = int(input())
     start_x, start_y = map(int, input().split())
     end_x, end_y = map(int, input().split())
     
-    visited = [ [0] * l for _ in range(l)]
-    dx = [2,2,1,-1,-2,-2,-1,1]
-    dy = [1,-1,2,2,1,-1,-2,-2]    
-    bfs(start_x, start_y)
-   
+    visited = [[0] * l for i in range(l)]
+    dx = [2,2,-2,-2,1,-1,1,-1]
+    dy = [1,-1,1,-1,2,2,-2,-2]
+    
+    ans = bfs(start_x, start_y)
+    print(ans)
+    
