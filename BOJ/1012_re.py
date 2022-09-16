40b6da929a41fa2248d39b755fcfd287d6521c7c
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-09-16 15:48:47 +0900

    Update 1012_re.py

--- BOJ/1012_re.py
+++ BOJ/1012_re.py
@@ -1,15 +1,40 @@
 import sys
 from collections import deque
 
-t = int(input())
-m, n, k = map(int, input().split())
-graph = [[0 for _ in range(m)] for i in range(n)]
-print(graph)
 
-# for i in range(t):
-#     for j in range(k):
-#         a, b = map(int, input().split())
-#         graph[a][b] = 1
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    
+    while q:
+        v = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1: 
+                q.append((nx, ny))
+                graph[nx][ny] = 2
+        
+    
+    
+    
+t = int(input())
+
+for i in range(t):
+    m, n, k = map(int, input().split())
+    graph = [[0 for _ in range(m)] for i in range(n)]
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    for j in range(k):
+        a, b = map(int, input().split())
+        graph[a][b] = 1
+    cnt = 0
+    
+    for i in range(n):
+        for j in range(m):
+            cnt += bfs()
         
         
     
