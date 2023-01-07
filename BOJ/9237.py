a0265dc3182ca9d1580df734deaf02aea497d106
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2023-01-07 19:19:28 +0900

    Add : 이장님 초대

---
+++ BOJ/9237.py
@@ -0,0 +1,13 @@
+# 이장님 초대
+
+n = int(input())
+t_list = list(map(int, input().split()))
+
+t_list.sort(reverse=True)
+
+growing_up = []
+for i in range(1, len(t_list)+1):
+    growing_up.append(i + t_list[i-1])
+    
+print(max(growing_up)+1)
+
